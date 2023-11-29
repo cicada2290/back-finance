17c4f901c410e8354042e0215102eadcdf4ff847
0xpokotaro <kentarou_masuda@ruckplus-tech.io>
2023-11-29 18:07:54 +0900

    ♻️ refactor

--- src/app/api/cryptocurrency/prices/route.ts
+++ src/app/api/cryptocurrency/prices/route.ts
@@ -1,28 +1,24 @@
-import Binance from 'binance-api-node'
 import { NextResponse } from 'next/server'
 
-const coins = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'XRPUSDT']
-
 export async function GET() {
-  const client = Binance({
-    apiKey: 'O14lstrxmuX0Zhv7Zk4pArdIFtH3yK7UjTz4QuhNwfSEFdXWLm6JWo5Utnx3jRJg',
-    apiSecret:
-      '7aZaNViAoMgf39fovzyLRsIEJPufV1m4Sg6Ibjgw3BD6Id2MINpYTqlRQxS0F7VD',
-    getTime: new Date().getTime,
+  return NextResponse.json({
+    data: [
+      {
+        currency: 'BTC',
+        price: '38311.64',
+      },
+      {
+        currency: 'ETH',
+        price: '2069.65',
+      },
+      {
+        currency: 'BNB',
+        price: '231.11',
+      },
+      {
+        currency: 'XRP',
+        price: '0.61',
+      },
+    ],
   })
-
-  const prices = await client.prices()
-
-  const res: {
-    [key: string]: string
-  } = Object.keys(prices)
-    .filter((key) => coins.includes(key))
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    .reduce((obj: any, key: string) => {
-      const newKey = key.replace('USDT', '')
-      obj[newKey] = prices[key]
-      return obj
-    }, {})
-
-  return NextResponse.json(res)
 }
